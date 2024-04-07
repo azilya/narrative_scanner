d8ca1383a169e71b2668cbfa1d79cd55ba248a15
I.A <azerkovich@apsolutions.ru>
2024-04-07 16:54:25 +0300

    added an input form

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 from utils import NarrativeModel
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
 
 app = FastAPI()
 
@@ -11,8 +17,14 @@ def hello():
 
 
 @app.post("/narrative_scanner")
-def scan(posts, question):
+def narrative_scanner(posts: str = Form(), question: str = Form()):
     inputs = posts.split("\n\n")
     model = NarrativeModel(inputs)
     reply = model.generate(question)
     return reply
+
+
+@app.get("/")
+@app.get("/form", response_class=HTMLResponse)
+def main(request: Request):
+    return templates.TemplateResponse("form.html", {"request": request})
